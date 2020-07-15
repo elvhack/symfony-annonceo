68a59c7f1b420b588412d4fb769804a8dac9a2a6
Elvan Eroglu <elvan.alwan@gmail.com>
2020-07-15 11:41:44 +0200

    fix errors, add correct user entity, register form

--- src/Entity/Utilisateur.php
+++ src/Entity/Utilisateur.php
@@ -3,14 +3,15 @@
 namespace App\Entity;
 
 use App\Repository\UtilisateurRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UtilisateurRepository::class)
+ * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
  */
-class Utilisateur
+class Utilisateur implements UserInterface
 {
     /**
      * @ORM\Id()
@@ -20,19 +21,26 @@ class Utilisateur
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=180, unique=true)
      */
     private $email;
 
     /**
-     * @ORM\Column(type="string", length=255, unique=true)
+     * @ORM\Column(type="json")
      */
-    private $pseudo;
+    private $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private $password;
 
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $mdp;
+    private $pseudo;
+
 
     /**
      * @ORM\Column(type="string", length=255)
@@ -54,28 +62,6 @@ class Utilisateur
      */
     private $inscription;
 
-    /**
-     * @ORM\OneToMany(targetEntity=Annonce::class, mappedBy="auteur")
-     */
-    private $annonces;
-
-    /**
-     * @ORM\ManyToMany(targetEntity=Commentaire::class, mappedBy="auteur")
-     */
-    private $commentaires;
-
-    /**
-     * @ORM\OneToMany(targetEntity=Note::class, mappedBy="auteur")
-     */
-    private $notes;
-
-    public function __construct()
-    {
-        $this->annonces = new ArrayCollection();
-        $this->commentaires = new ArrayCollection();
-        $this->notes = new ArrayCollection();
-    }
-
     public function getId(): ?int
     {
         return $this->id;
@@ -93,6 +79,67 @@ class Utilisateur
         return $this;
     }
 
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
     public function getPseudo(): ?string
     {
         return $this->pseudo;
@@ -105,17 +152,7 @@ class Utilisateur
         return $this;
     }
 
-    public function getMdp(): ?string
-    {
-        return $this->mdp;
-    }
 
-    public function setMdp(string $mdp): self
-    {
-        $this->mdp = $mdp;
-
-        return $this;
-    }
 
     public function getPrenom(): ?string
     {
@@ -161,96 +198,6 @@ class Utilisateur
     public function setInscription(\DateTimeInterface $inscription): self
     {
         $this->inscription = $inscription;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|Annonce[]
-     */
-    public function getAnnonces(): Collection
-    {
-        return $this->annonces;
-    }
-
-    public function addAnnonce(Annonce $annonce): self
-    {
-        if (!$this->annonces->contains($annonce)) {
-            $this->annonces[] = $annonce;
-            $annonce->setAuteur($this);
-        }
-
-        return $this;
-    }
-
-    public function removeAnnonce(Annonce $annonce): self
-    {
-        if ($this->annonces->contains($annonce)) {
-            $this->annonces->removeElement($annonce);
-            // set the owning side to null (unless already changed)
-            if ($annonce->getAuteur() === $this) {
-                $annonce->setAuteur(null);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|Commentaire[]
-     */
-    public function getCommentaires(): Collection
-    {
-        return $this->commentaires;
-    }
-
-    public function addCommentaire(Commentaire $commentaire): self
-    {
-        if (!$this->commentaires->contains($commentaire)) {
-            $this->commentaires[] = $commentaire;
-            $commentaire->addAuteur($this);
-        }
-
-        return $this;
-    }
-
-    public function removeCommentaire(Commentaire $commentaire): self
-    {
-        if ($this->commentaires->contains($commentaire)) {
-            $this->commentaires->removeElement($commentaire);
-            $commentaire->removeAuteur($this);
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|Note[]
-     */
-    public function getNotes(): Collection
-    {
-        return $this->notes;
-    }
-
-    public function addNote(Note $note): self
-    {
-        if (!$this->notes->contains($note)) {
-            $this->notes[] = $note;
-            $note->setAuteur($this);
-        }
-
-        return $this;
-    }
-
-    public function removeNote(Note $note): self
-    {
-        if ($this->notes->contains($note)) {
-            $this->notes->removeElement($note);
-            // set the owning side to null (unless already changed)
-            if ($note->getAuteur() === $this) {
-                $note->setAuteur(null);
-            }
-        }
 
         return $this;
     }
