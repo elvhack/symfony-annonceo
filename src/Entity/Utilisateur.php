bffc3174c36f872d7c950024dca6d746362168ae
Elvan Eroglu <elvan.alwan@gmail.com>
2020-07-15 11:09:28 +0200

    unique email

--- src/Entity/Utilisateur.php
+++ src/Entity/Utilisateur.php
@@ -25,7 +25,7 @@ class Utilisateur
     private $email;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
      */
     private $pseudo;
 
